968029436c5d311cd4d6203dde3526df0cc55cf3
Jam <133597316+hoppityhop@users.noreply.github.com>
2024-03-31 16:20:00 -0500

    Added build and Docker requirements.

---
+++ demo/src/main/java/com/example/demo/RestController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@org.springframework.web.bind.annotation.RestController
+@RequestMapping("api/v1/cities")
+public class RestController {
+    private static final List<Country> COUNTRIES = List.of(
+            new Country("United States of America", "Washington D.C.", 339_996_563),
+            new Country("China", "Beijing", 1_411_750_000),
+            new Country("India", "New Delhi", 1_428_627_663)
+    );
+
+    @GetMapping
+    public List<Country> getCountries() {
+        return COUNTRIES;
+    }
+}
